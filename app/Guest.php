d3ae3ed9722deeab65019403bc5ef1125ad1d840
Ezra <johnezrapaz@gmail.com>
2019-06-13 15:29:16 +0800

    fillGuest function created

--- app/Guest.php
+++ app/Guest.php
@@ -8,4 +8,10 @@ class Guest extends Model
 {
     protected $guarded = ['created_at', 'updated_at'];
 
+    public function fillGuest($attributes = []){
+        if($this->fill($attributes)){
+            return true;
+        }
+        return false;
+    }
 }
