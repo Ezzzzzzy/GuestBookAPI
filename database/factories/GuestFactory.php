128e08aef93666b2474b96e1c2f3552e6e41058a
Ezra <johnezrapaz@gmail.com>
2019-06-13 15:23:04 +0800

    Guest migration,factory,model created

---
+++ database/factories/GuestFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Guest;
+use Faker\Generator as Faker;
+
+$factory->define(Guest::class, function (Faker $faker) {
+    return [
+        'first_name' => $faker->firstName,
+        'last_name' => $faker->lastName,
+        'last_name' => $faker->lastName,
+        'email' => $faker->email,
+        'number' => $faker->e164PhoneNumber,
+        'gender' => $faker->randomElement($array = array ('Male','Female',)),
+        'Address' => $faker->address 
+    ];
+});
